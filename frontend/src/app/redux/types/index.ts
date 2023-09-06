e65fa8f9ae3aadc5a1fb5f14f1e2b85fd7d412c1
Gleb001 <lichagingleb@mail.ru>
2023-09-06 19:47:15 +0300

    Add JEST testing library

---
+++ frontend/src/app/redux/types/index.ts
@@ -0,0 +1,14 @@
+// imports =================================================== //
+import {store} from "..";
+
+// main ====================================================== //
+type RootStore = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+interface initialState<T> {
+    data: T,
+    status: 'pending' | 'loading' | 'succeeded' | 'failed',
+    error: string,
+}
+
+// export ==================================================== //
+export type { RootStore, AppDispatch, initialState };
